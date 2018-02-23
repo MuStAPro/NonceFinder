4aee614d9da0fc17c608ca18fffc15b0fb440c5d
Mustafa Al-Ameen <mustaprox@gmail.com>
2018-02-23 13:54:33 +0300

    Add files via upload

---
+++ NonceFinder/NonceFinder/MainForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace NonceFinder
+{
+    partial class NonceFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtNonce = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblpath = new System.Windows.Forms.Label();
+            this.lblnonce = new System.Windows.Forms.Label();
+            this.open = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(109, 34);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(241, 20);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // txtNonce
+            // 
+            this.txtNonce.Location = new System.Drawing.Point(109, 63);
+            this.txtNonce.Name = "txtNonce";
+            this.txtNonce.ReadOnly = true;
+            this.txtNonce.Size = new System.Drawing.Size(293, 20);
+            this.txtNonce.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(356, 34);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(46, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblpath
+            // 
+            this.lblpath.AutoSize = true;
+            this.lblpath.Location = new System.Drawing.Point(13, 34);
+            this.lblpath.Name = "lblpath";
+            this.lblpath.Size = new System.Drawing.Size(90, 13);
+            this.lblpath.TabIndex = 3;
+            this.lblpath.Text = "SHSH2 File Path:";
+            // 
+            // lblnonce
+            // 
+            this.lblnonce.AutoSize = true;
+            this.lblnonce.Location = new System.Drawing.Point(12, 63);
+            this.lblnonce.Name = "lblnonce";
+            this.lblnonce.Size = new System.Drawing.Size(42, 13);
+            this.lblnonce.TabIndex = 4;
+            this.lblnonce.Text = "Nonce:";
+            // 
+            // open
+            // 
+            this.open.FileName = "Browse for shsh2 file";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(414, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // NonceFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 92);
+            this.Controls.Add(this.lblnonce);
+            this.Controls.Add(this.lblpath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtNonce);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "NonceFinder";
+            this.Text = "Nonce Finder";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtNonce;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblpath;
+        private System.Windows.Forms.Label lblnonce;
+        private System.Windows.Forms.OpenFileDialog open;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ NonceFinder/NonceFinder/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace NonceFinder
+{
+    public partial class NonceFinder : Form
+    {
+        public NonceFinder()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("NonceFiner is an application designed to quickly find the nonce value from an shsh2 file.\r\nBy /u/mustapro and designed for /r/jailbreak.");
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = open.FileName;
+                    txtFilePath.Text = filePath;
+                    txtNonce.Text = getNonce(filePath);
+                }
+            }
+            catch { }
+        }
+
+        private string getNonce(string filePath)
+        {
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load(filePath);
+
+                if (!(xml == null))
+                {
+                    string nodeStr = "";
+                    XmlNodeList nodeList = xml.GetElementsByTagName("string");
+
+                    foreach (XmlNode node in nodeList)
+                    {
+                        if (node.InnerXml.StartsWith("0x"))
+                        {
+                            nodeStr = node.InnerXml;
+                        }
+                    }
+                    return nodeStr;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            catch {
+                return "";
+            }
+        }
+    }
+}
